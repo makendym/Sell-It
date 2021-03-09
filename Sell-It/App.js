493bf4c1c51a65e9dde7d20890e3c5ce8b660fe0
makendym <mmidouin18@stac.edu>
2021-03-09 14:32:21 -0500

    Added all the necessary files

---
+++ Sell-It/App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+
+export default function App() {
+  return <WelcomeScreen />;
+}
